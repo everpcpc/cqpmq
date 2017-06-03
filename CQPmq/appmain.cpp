b097724cebee120177fa80cce9843c99d00acc5f
everpcpc <git@everpcpc.com>
2017-06-04 03:07:34 +0800

    add read_config

--- CQPmq/appmain.cpp
+++ CQPmq/appmain.cpp
@@ -22,22 +22,60 @@ int64_t qq = -1;
 char log_buf[1024];
 
 Client btdclient;
-string mq_host = "saki.everpcpc.com";
-int mq_port = 11300;
+char* SERVER_ADDR = "127.0.0.1";
+int SERVER_PORT = 11300;
 
 string in_q = "coolq_in";
 string out_q = "coolq_out";
 
 
-int send_to_mq(char* msg) {
+int send_to_mq(const char* msg) {
 	if (!btdclient.is_connected()) {
-		CQ_addLog(ac, CQLOG_WARNING, "连接", "未连接");
+		CQ_addLog(ac, CQLOG_WARNING, "net", "未连接");
 		return -1;
 	}
 	sprintf_s(log_buf, "发送: %s", msg);
-	CQ_addLog(ac, CQLOG_DEBUG, "消息", log_buf);
+	CQ_addLog(ac, CQLOG_DEBUG, "info", log_buf);
 	int64_t id = btdclient.put(msg);
-	free(msg);
+	return 0;
+}
+
+int read_config() {
+	string configFolder = ".\\app\\" CQAPPID;
+	string configFile = configFolder + "\\config.ini";
+
+	if (GetFileAttributesA(configFile.data()) == -1) {
+		if (GetFileAttributesA(configFolder.data()) == -1) {
+			CreateDirectoryA(configFolder.data(), NULL);
+		}
+		CloseHandle(CreateFileA(configFile.data(), GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL));
+		CQ_addLog(ac, CQLOG_INFO, "info", "配置文件不存在，以默认值自动生成");
+	}
+
+	int port = GetPrivateProfileIntA("btd", "port", -1, configFile.data());
+
+	if (port != -1) {
+		SERVER_PORT = port;
+	}
+	else {
+		string value;
+		stringstream ss;
+		ss << SERVER_PORT;
+		ss >> value;
+		WritePrivateProfileStringA("btd", "port", value.data(), configFile.data());
+		ss.clear();
+		value.clear();
+	}
+
+	char *buffer = new char[256];
+	GetPrivateProfileStringA("btd", "addr", "localhost", buffer, 64, configFile.data());
+	if (strcmp("localhost", buffer)) {
+		SERVER_ADDR = buffer;
+	}
+	else {
+		WritePrivateProfileStringA("btd", "addr", SERVER_ADDR, configFile.data());
+	}
+	delete[] buffer;
 	return 0;
 }
 
@@ -66,7 +104,7 @@ CQEVENT(int32_t, Initialize, 4)(int32_t AuthCode) {
 */
 CQEVENT(int32_t, __eventStartup, 0)() {
 
-	CQ_addLog(ac, CQLOG_DEBUG, "提示", "插件启动啦喵~");
+	CQ_addLog(ac, CQLOG_DEBUG, "info", "插件启动啦喵~");
 
 	return 0;
 }
@@ -79,7 +117,7 @@ CQEVENT(int32_t, __eventStartup, 0)() {
 */
 CQEVENT(int32_t, __eventExit, 0)() {
 
-	CQ_addLog(ac, CQLOG_DEBUG, "连接", "断开连接，886~");
+	CQ_addLog(ac, CQLOG_DEBUG, "net", "断开连接，886~");
 	btdclient.disconnect();
 
 	return 0;
@@ -94,28 +132,30 @@ CQEVENT(int32_t, __eventExit, 0)() {
 CQEVENT(int32_t, __eventEnable, 0)() {
 	enabled = true;
 
+	read_config();
+
 	qq = CQ_getLoginQQ(ac);
 	if (qq < 0) {
-		CQ_addLog(ac, CQLOG_ERROR, "提示", "获取不到已登录的QQ号呢");
+		CQ_addLog(ac, CQLOG_ERROR, "info", "获取不到已登录的QQ号呢");
 		return -1;
 	}
 	sprintf_s(log_buf, "登录的 QQ 号为: %" PRId64, qq);
-	CQ_addLog(ac, CQLOG_DEBUG, "提示", log_buf);
+	CQ_addLog(ac, CQLOG_DEBUG, "info", log_buf);
 
-	sprintf_s(log_buf, "尝试连接：%s:%d...", mq_host.c_str(), mq_port);
-	CQ_addLog(ac, CQLOG_DEBUG, "连接", log_buf);
+	sprintf_s(log_buf, "尝试连接：%s:%d...", SERVER_ADDR, SERVER_PORT);
+	CQ_addLog(ac, CQLOG_DEBUG, "net", log_buf);
 	try {
-		btdclient.connect(mq_host, mq_port);
+		btdclient.connect(SERVER_ADDR, SERVER_PORT);
 	}
 	catch (runtime_error) {
-		CQ_addLog(ac, CQLOG_ERROR, "连接", "连不上，地址写错了喵？");
+		CQ_addLog(ac, CQLOG_ERROR, "net", "连不上，地址写错了喵？");
 		return -1;
 	}
 	
 	out_q = to_string(qq) + "_out";
 	in_q = to_string(qq) + "_in";
 	sprintf_s(log_buf, "连接成功，使用：%s，监听：%s", out_q.c_str(), in_q.c_str());
-	CQ_addLog(ac, CQLOG_DEBUG, "连接", log_buf);
+	CQ_addLog(ac, CQLOG_DEBUG, "net", log_buf);
 	btdclient.use(out_q);
 	btdclient.watch(in_q);
 
@@ -131,7 +171,7 @@ CQEVENT(int32_t, __eventEnable, 0)() {
 */
 CQEVENT(int32_t, __eventDisable, 0)() {
 	enabled = false;
-	CQ_addLog(ac, CQLOG_DEBUG, "提示", "停用了，断开连接喵～");
+	CQ_addLog(ac, CQLOG_DEBUG, "info", "停用了，断开连接喵～");
 	btdclient.disconnect();
 	return 0;
 }
@@ -145,18 +185,15 @@ CQEVENT(int32_t, __eventPrivateMsg, 24)(int32_t subType, int32_t sendTime, int64
 
 	//如果要回复消息，请调用酷Q方法发送，并且这里 return EVENT_BLOCK - 截断本条消息，不再继续处理  注意：应用优先级设置为"最高"(10000)时，不得使用本返回值
 	//如果不回复消息，交由之后的应用/过滤器处理，这里 return EVENT_IGNORE - 忽略本条消息
-	int32_t msg_len = sizeof(msg);
-	int32_t msg_type = 21;
-	char *msg_buf = new char[4 + 4 + 4 + 8 + msg_len + 4];
+	stringstream ss;
+	ss << 21 << '\36';
+	ss << subType << '\36';
+	ss << sendTime << '\36';
+	ss << fromQQ << '\36';
+	ss << msg << '\36';
+	ss << font << '\36';
 
-	memcpy(msg_buf, &msg_type, 4);
-	memcpy(msg_buf + 4, &subType, 4);
-	memcpy(msg_buf + 4 + 4, &sendTime, 4);
-	memcpy(msg_buf + 4 + 4 + 4, &fromQQ, 8);
-	memcpy(msg_buf + 4 + 4 + 4 + 8, &msg, msg_len);
-	memcpy(msg_buf + 4 + 4 + 4 + 8 + msg_len, &font, 4);
-
-	send_to_mq(msg_buf);
+	send_to_mq(ss.str().c_str());
 
 	return EVENT_IGNORE;
 }
